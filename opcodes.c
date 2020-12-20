ca7b07bcb2d9dc2a8a677182e86d0e152366dff7
mauricio.sierrac <2376@holbertonschool.com>
2020-12-20 22:17:48 +0000

    upload task 14 :sunglasses:

--- opcodes.c
+++ opcodes.c
@@ -24,5 +24,26 @@ void rotl(stack_t **stack, unsigned int line_number)
 	temp->next->next = NULL;
 	temp->next->prev = temp;
 	(*stack)->prev = NULL;
+}
 
+void rotr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp, *temp2;
+	(void)line_number;
+
+	if (*stack == NULL || stack == NULL)
+	{
+		nop(stack, line_number);
+	}
+	
+	temp = (*stack)->next;
+	temp2 = (*stack)->next;
+
+	while (temp2->next != NULL)
+		temp2 = temp2->next;
+	temp2->prev->next = NULL;
+	(*stack)->next = temp2;
+	temp2->prev = *stack;
+	temp2->next = temp;
+	temp->prev = temp2;
 }
