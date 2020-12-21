561dceb0b2e7cff49c15bc9146eeb8afb975f43f
mauricio.sierrac <2376@holbertonschool.com>
2020-12-21 21:33:18 +0000

    fixed push

--- monty.h
+++ monty.h
@@ -45,7 +45,7 @@ typedef struct instruction_s
 /* PROTOTYPES */
 int buil_func(char *opcode, stack_t **stack, unsigned int line_number);
 void push(stack_t **stack, unsigned int line_number, char *n);
-void _pall(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
