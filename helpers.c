a4ae3122980749b9300cdcc1f8eda69450d19a47
mauricio.sierrac <2376@holbertonschool.com>
2020-12-21 21:32:36 +0000

    fixed push

--- helpers.c
+++ helpers.c
@@ -6,17 +6,17 @@
  * @line_number: line number of instruction
  * Return: void
  */
-void _pall(stack_t **stack, unsigned int line_number)
+void pall(stack_t **stack, unsigned int __attribute__((unused)) line_number)
 {
 	stack_t *path = *stack;
 
-	while (path)
+	if (*stack == NULL)
+		return;
+	while (path != NULL)
 	{
 		printf("%d\n", path->n);
 		path = path->next;
 	}
-
-	(void)line_number;
 }
 
 /**
