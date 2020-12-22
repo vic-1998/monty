8af8eca62febc1e333a458259236885705a883ec
mauricio.sierrac <2376@holbertonschool.com>
2020-12-22 00:14:36 +0000

    fix error name function

--- opcodes.c
+++ opcodes.c
@@ -23,7 +23,7 @@ void rotl(stack_t **stack, unsigned int line_number)
 }
 
 /**
- * rotr - rotates stack to right
+ * op_rotr - rotates stack to right
  * @stack: pointer to the head node pointer of stack
  * @line_number: the line number
  * Return: Nothing.
